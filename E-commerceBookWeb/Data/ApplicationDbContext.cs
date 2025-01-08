bb51c191306dcede243a78df363bab7cd1ca356d
Rakibul Islam <ratul997@hotmail.com>
2025-01-09 00:01:42 +0600

    updated

---
+++ E-commerceBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using E_commerceBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerceBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories{ get; set; }
+    }
+}
