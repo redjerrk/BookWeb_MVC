9a625f9d284acb9b91948688fcc26cc3256a9952
Rakibul Islam <ratul997@hotmail.com>
2025-01-10 22:24:03 +0600

    updated Nav and Footer

---
+++ E-commerceBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using E_commerceBookWeb.Data;
+using E_commerceBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerceBookWeb.Controllers
+{
+    public class CategoryController(ApplicationDbContext db) : Controller
+    {
+
+        private readonly ApplicationDbContext _db = db;
+
+        public IActionResult Index()
+        {
+
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
